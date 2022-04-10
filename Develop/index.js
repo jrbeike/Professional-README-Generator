e71216c48bd3d1bffbbcee74e08eaad993a5a1a6
jrbeike <jrbeike@gmail.com>
2022-04-10 16:31:38 -0400

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,26 @@
 const inquirer = require('inquirer');
 const fs = require('fs')  
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt(
+        [
+            {
+                type: 'input',
+                message ="Whats the project title?",
+                name: 'title';
+                vlaidate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    }   else {
+                        console.log ('Please enter your project title!');
+                        return false;
+                    }
+                }
+            }
+        ]
+    )
+    
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
